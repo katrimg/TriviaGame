9b706be495598b27ef490e99b8699a92111b0dd9
Katri Morrison-Goulias <katrimorrison-goulias@Konstadinoss-MacBook-Air.local>
2018-06-07 12:46:06 -0700

    first draft

---
+++ app.js
@@ -0,0 +1,20 @@
+//You'll create a trivia form with multiple choice or true/false options (your choice).
+
+//The player will have a limited amount of time to finish the quiz. 
+
+//The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+//Don't let the player pick more than one answer per question.
+
+//Don't forget to include a countdown timer.
+
+//pseudo coding
+//The player has 120 seconds to answer the questions
+//if the player answers the first question right, increase wins by 1
+//if the player answers the second questions right, increase losses by 1
+
+
+
+
+
+    
